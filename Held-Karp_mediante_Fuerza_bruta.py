0ec7d7148fc7689e03c15932f385b3348d5a34d8
Mateo2475 <mmaguina24@gmail.com>
2026-07-05 22:39:27 -0500

    Update Held-Karp_mediante_Fuerza_bruta.py

--- Held-Karp_mediante_Fuerza_bruta.py
+++ Held-Karp_mediante_Fuerza_bruta.py
@@ -1,54 +1,48 @@
-def permutar(nivel, costoAcumulado, n, distancias, visitado, rutaActual, mejorRuta, costoMinimo):
+def permutar(nivel, costo_acumulado, n, distancias, visitado, ruta_actual, mejor_ruta, costo_minimo):
     if nivel == n:
-        nuevoCosto = costoAcumulado + distancias[rutaActual[n-1]][rutaActual[0]]
-        if nuevoCosto < costoMinimo[0]:
-            costoMinimo[0] = nuevoCosto
-            
-            mejorRuta[:] = list(rutaActual)
+        nuevo_costo = costo_acumulado + distancias[ruta_actual[n-1]][ruta_actual[0]]
+        if nuevo_costo < costo_minimo[0]:
+            costo_minimo[0] = nuevo_costo
+            mejor_ruta[:] = list(ruta_actual)
     else:
         for i in range(1, n):  
             if not visitado[i]:
                 visitado[i] = True
-                rutaActual[nivel] = i
-                
+                ruta_actual[nivel] = i
                 
                 permutar(
                     nivel + 1, 
-                    costoAcumulado + distancias[rutaActual[nivel-1]][i], 
-                    n, distancias, visitado, rutaActual, mejorRuta, costoMinimo
+                    costo_acumulado + distancias[ruta_actual[nivel-1]][i], 
+                    n, distancias, visitado, ruta_actual, mejor_ruta, costo_minimo
                 )
                 
-                visitado[i] = False  
+                visitado[i] = False
 
 def main():
-    n = int(input("ingrese el numero de ciudades: "))
-    
+    n = int(input("Ingrese el numero de ciudades: "))
     
     distancias = [[0] * n for _ in range(n)]
     
-   
     for i in range(n):
         for j in range(n):
             if i != j:
-                distancias[i][j] = int(input(f"distancia de ciudad {i+1} a ciudad {j+1}: "))
+                distancias[i][j] = int(input(f"Distancia de ciudad {i+1} a ciudad {j+1}: "))
                 
     visitado = [False] * n
-    rutaActual = [0] * n
-    mejorRuta = [0] * n
+    ruta_actual = [0] * n
+    mejor_ruta = [0] * n
     
-   
-    costoMinimo = [999999] 
+    costo_minimo = [999999] 
     visitado[0] = True
-    rutaActual[0] = 0
-    permutar(1, 0, n, distancias, visitado, rutaActual, mejorRuta, costoMinimo)
+    ruta_actual[0] = 0
+    permutar(1, 0, n, distancias, visitado, ruta_actual, mejor_ruta, costo_minimo)
     
-    print("\nla ruta optima es:")
-   
-    for ciudad in mejorRuta:
+    print("\nLa ruta optima es:")
+    for ciudad in mejor_ruta:
         print(ciudad + 1)
         
-    print(f"el costo minimo es: {costoMinimo[0]}")
+    print(f"El costo minimo es: {costo_minimo[0]}")
 
-
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
+
